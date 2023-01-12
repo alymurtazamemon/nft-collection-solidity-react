7bb1f87d9920f2cafca35bfabbc5d364baedf6c2
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-12 22:35:51 +0500

    install hardhat deploy dependency

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
+import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
