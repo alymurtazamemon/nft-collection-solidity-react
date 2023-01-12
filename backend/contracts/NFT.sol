ef2569cf23533fa2ccdbe0219dc320b97c65a695
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-12 15:38:27 +0500

    extend ERC721URIStorage contract and implement constructor

--- backend/contracts/NFT.sol
+++ backend/contracts/NFT.sol
@@ -1,5 +1,24 @@
 // SPDX-License-Identifier: MIT
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
 pragma solidity ^0.8.17;
 
-contract NFT {}
+contract NFT is ERC721URIStorage {
+    // * STATE VARIABLES
+    string private _nftTokenUri;
+    uint256 private _mintFee;
+    uint256 private _tokenCounter;
+
+    // * FUNCTIONS
+    constructor(
+        string memory nftTokenUri,
+        string memory name,
+        string memory symbol,
+        uint256 mintFee
+    ) ERC721(name, symbol) {
+        _nftTokenUri = nftTokenUri;
+        _mintFee = mintFee;
+        _tokenCounter = 0;
+    }
+}
