6387af6f5e8cddef9ad266e7e5a5301612b0c8be
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-17 19:48:53 +0500

    add title, subtile, cta, and backgroung

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
+import "./App.css";
+
 function App() {
-    return <div>Hello world</div>;
+    return (
+        <div>
+            <h1>Gold Sonic Cat Collection</h1>
+            <h2>Mint Yours Now!</h2>
+            <button>Mint</button>
+        </div>
+    );
 }
 
 export default App;
