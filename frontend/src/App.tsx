27d4c77c3fb1d59af980b78c42da023d64504a29
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-18 12:02:12 +0500

    add loading indicator

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { ConnectButton } from "@web3uikit/web3";
 import { useMoralis, useWeb3Contract } from "react-moralis";
-import { useEffect } from "react";
-import { ContractTransaction, ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { ContractInterface, ContractTransaction, ethers } from "ethers";
 import "./App.css";
 
 import { contractAddresses, abi } from "./constants";
 import { AiFillBell } from "react-icons/ai";
-import { useNotification } from "@web3uikit/core";
+import { Loading, useNotification } from "@web3uikit/core";
 
 interface contractAddressesInterface {
     [key: string]: string[];
@@ -20,6 +20,8 @@ function App() {
         chainId in addresses ? addresses[chainId][0] : null;
 
     const dispatch = useNotification();
+
+    let [loading, setLoading] = useState(false);
 
     const { runContractFunction: mintNft } = useWeb3Contract({
         abi: abi,
@@ -46,8 +48,48 @@ function App() {
     }
 
     async function handleSuccess(tx: ContractTransaction): Promise<void> {
+        console.log("success");
         await tx.wait(1);
-        alert("success");
+        listenEvent();
+    }
+
+    function timeout(delay: number) {
+        return new Promise((res) => setTimeout(res, delay));
+    }
+
+    async function listenEvent() {
+        console.log("listening");
+        setLoading(true);
+
+        // * add a waiting delay for hardhat network.
+        if (chainId == "31337") {
+            await timeout(10000);
+        }
+
+        try {
+            const { ethereum } = window as any;
+
+            if (ethereum) {
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                const connectedContract = new ethers.Contract(
+                    nftContractAddress!,
+                    abi as ContractInterface,
+                    signer
+                );
+
+                connectedContract.once("NFTMinted", (from, tokenId, value) => {
+                    alert(
+                        `NFT minted successfully. You can view the NFT on Opensea testnet: https://testnets.opensea.io/assets/goerli/${nftContractAddress}/${tokenId}`
+                    );
+                });
+
+                console.log("done");
+                setLoading(false);
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -70,12 +112,24 @@ function App() {
             <div className="mt-36 text-white">
                 {isWeb3Enabled ? (
                     nftContractAddress ? (
-                        <button
-                            onClick={handleOnMint}
-                            className="bg-gradient-to-r from-[#60c657] to-[#35aee2] px-24 py-4 text-xl text-white font-bold rounded-lg"
-                        >
-                            Mint
-                        </button>
+                        <div className="flex flex-col items-center">
+                            <button
+                                onClick={handleOnMint}
+                                className="bg-gradient-to-r from-[#60c657] to-[#35aee2] px-24 py-4 text-xl text-white font-bold rounded-lg mb-24"
+                            >
+                                Mint
+                            </button>
+                            {loading && (
+                                <Loading
+                                    direction="bottom"
+                                    fontSize={16}
+                                    size={12}
+                                    spinnerColor="#35aee2"
+                                    spinnerType="wave"
+                                    text="Transaction pending..."
+                                />
+                            )}
+                        </div>
                     ) : (
                         <p>
                             This network is not supported, please switch to
