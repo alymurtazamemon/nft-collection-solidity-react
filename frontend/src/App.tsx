d6406de6af0061e22078c3692b9c268c54644669
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-01-18 09:22:12 +0500

    install moralis provider and web3uikit and add connect button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { ConnectButton } from "@web3uikit/web3";
 import "./App.css";
 
 function App() {
     return (
         <div className="flex flex-col justify-start items-center h-screen">
-            <h1 className="font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-[rgb(96,198,87)] to-[#35aee2] w-fit mt-12">
-                Gold Sonic Cat
-            </h1>
+            <div className="flex items-center mt-12 w-full">
+                <div className="w-1/3"></div>
+                <h1 className="font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-[rgb(96,198,87)] to-[#35aee2]">
+                    Gold Sonic Cat
+                </h1>
+                <div className="ml-auto mr-8">
+                    <ConnectButton />
+                </div>
+            </div>
             <h2 className="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-[#60c657] to-[#35aee2] w-fit mt-4">
                 NFT Collection
             </h2>
